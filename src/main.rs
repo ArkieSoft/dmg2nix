c7e4eed332c655e5967e65b3dc9558be5a4ef2d7
ArkieSoft <wyattsunkel@ArkMac.local>
2024-02-10 15:51:51 -0800

    new code with advice

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,10 @@ use download_rs::async_download::Download;
 use glob::glob;
 use dmg::Attach;
 use std::error::Error;
+use std::io::Write;
 //use std::ffi::OsString;
-mod write;
-use write::write;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     let dmgname = &args[1];
@@ -26,14 +25,16 @@ fn main() {
         .read_line(&mut name)
         .expect("Failed to read line");
 
+    name = name.trim().to_owned();
 
     let download = Download::new(dmgname,Some("target.dmg"),None);
 
     match download.download() {
-        Ok(_) => println!("Downloaded!"),
-        Err(e) => println!("Failure"),
-    };
+    Ok(_) => println!("Downloaded"),
+    Err(e) => println!("Failure"),
 
+    }
+    
     println!("Version");
     let mut version = String::new();
 
@@ -56,7 +57,7 @@ fn main() {
     let last = appnamesplit.last().unwrap();
     println!("Found .app Directory");
     println!("{:?}", last);
-
+    
     println!("Hashing");
     let _prehash = Command::new("nix")
         .arg("hash")
@@ -68,19 +69,46 @@ fn main() {
         .output()
         .expect("Failed Command");
 
+    //println!("{:?}", _prehash);
+
   //  let _prehashstr = _prehash.stdout;
 
-    let hash = match std::str::from_utf8(&_prehash.stdout) {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    };
+//    let hash = match std::str::from_utf8(&_prehash.stdout) {
+//        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+//        Ok(_) => todo!(),
+//    };
     println!("Hashing Complete");
-    println!("{}", hash);
-
-    let nixname = ("{}-darwin.nix", name.clone());
-
-    write();
-
+  //  println!("{:?}", hash);
+  //
+    let content_str: String = format!(r#"{{ stdenv, undmg }}:
+                 
+stdenv.mkDerivation rec {{
+    pname = "{0}";
+    version = "{1}";
+    src = pkgs.fetchurl {{
+        recursiveHash = true;
+        url = "{2}";
+        hash = "{3:?}";
+        name = "target.dmg"
+    }};
+                     
+                     
+    buildInputs = [pkgs.undmg];
+                     
+    unpackPhase = ''
+        undmg $src
+    '';
+                     
+    installPhase = ''
+        ls
+        mkdir -p #out/Applications/
+        cp -r {0}.app $out/Applications/
+    '';
+}}"#, name, version, dmgname, _prehash);
+    let content = content_str.as_bytes();
+    let mut f = File::create("target.nix");
+    f?.write_all(content);
+    Ok(())
 
 }
 
